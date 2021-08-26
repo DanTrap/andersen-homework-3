e0ab11633923ee1f86fca0390678a5454a94628d
DanTrap <danntrp@gmail.com>
2021-08-26 23:25:38 +0300

    downloading image with HttpURLConnection and Java threads

--- app/src/main/java/com/dantrap/andersen_homework_3/MainActivity.kt
+++ app/src/main/java/com/dantrap/andersen_homework_3/MainActivity.kt
@@ -1,21 +1,16 @@
 package com.dantrap.andersen_homework_3
 
-import android.content.Context
-import android.graphics.drawable.Drawable
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.webkit.URLUtil
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doAfterTextChanged
 import androidx.core.widget.doOnTextChanged
-import androidx.swiperefreshlayout.widget.CircularProgressDrawable
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.target.Target
 import com.dantrap.andersen_homework_3.databinding.ActivityMainBinding
+import java.io.BufferedInputStream
+import java.io.IOException
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,10 +21,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-
         binding.urlEditText.doOnTextChanged { text, _, _, _ ->
-
             if (URLUtil.isValidUrl(text.toString())) {
                 binding.textField.error = null
                 loadImage(text.toString())
@@ -37,63 +29,37 @@ class MainActivity : AppCompatActivity() {
                 binding.textField.error = "Invalid URL"
                 binding.imageView.setImageResource(0)
             }
-
         }
 
         binding.urlEditText.doAfterTextChanged { text ->
             if (text.isNullOrEmpty()) {
-                with (binding) {
+                with(binding) {
                     textField.error = null
                 }
             }
         }
     }
 
-    private fun loadImage(url: String) {
-
-        Glide
-            .with(this)
-            .load(url)
-            .listener(object : RequestListener<Drawable> {
-
-                override fun onLoadFailed(
-                    e: GlideException?,
-                    model: Any?,
-                    target: Target<Drawable>?,
-                    isFirstResource: Boolean
-                ): Boolean {
-                    Toast.makeText(this@MainActivity, "Loading failed", Toast.LENGTH_SHORT).show()
-                    return false
+    private fun loadImage(imageUrl: String) {
+        val thread = Thread {
+            val url = URL(imageUrl)
+            val urlConnection = url.openConnection() as HttpURLConnection
+            try {
+                val inputStream = BufferedInputStream(urlConnection.inputStream)
+                val bitmap = BitmapFactory.decodeStream(inputStream)
+                runOnUiThread {
+                    binding.imageView.setImageBitmap(bitmap)
                 }
-
-                override fun onResourceReady(
-                    resource: Drawable?,
-                    model: Any?,
-                    target: Target<Drawable>?,
-                    dataSource: DataSource?,
-                    isFirstResource: Boolean
-                ): Boolean {
-                    return false
+            } catch (e: IOException) {
+                e.printStackTrace()
+                runOnUiThread {
+                    binding.imageView.setImageResource(R.drawable.ic_warning)
                 }
-
-            })
-            .error(R.drawable.ic_warning)
-            .transition(DrawableTransitionOptions.withCrossFade(500))
-            .placeholder(getCircularProgress(this))
-            .into(binding.imageView)
-    }
-
-
-
-    companion object {
-
-        private fun getCircularProgress(context: Context) : CircularProgressDrawable{
-            val circularProgressDrawable = CircularProgressDrawable(context)
-            circularProgressDrawable.strokeWidth = 5f
-            circularProgressDrawable.centerRadius = 30f
-            circularProgressDrawable.start()
-            return circularProgressDrawable
+            } finally {
+                urlConnection.disconnect()
+            }
         }
+        thread.start()
     }
 }
 
